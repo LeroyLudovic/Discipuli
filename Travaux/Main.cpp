53ca7f154913ea8a7375faaba874cbf0cec43734
Evan <evan.doxanis@etu.univ-lyon1.fr>
2022-03-24 16:10:17 +0100

    salut bg

--- Travaux/Main.cpp
+++ Travaux/Main.cpp
@@ -1,7 +1,16 @@
 #include "Vecteur.h"
+#include <iostream>
+using namespace std;
 
 int main(){
     Vecteur v1;
     Vecteur v2(5,6);
+    v1.x =3;
+    v1.y =6;
 
+    cout<<v1<<endl;
+
+    v1 = v1+v2;
+
+    cout<<v1<<endl;
 }
